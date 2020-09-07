dbcce2e9bd3a7bec5452d76a4152c93cb51f9c0a
Nícolas Roque <nicolas.rsantos1@gmail.com>
2020-09-07 19:45:47 -0300

    Funções de treino e teste criadas

--- adaline/adaline.py
+++ adaline/adaline.py
@@ -10,34 +10,56 @@ def read_files(dir):
             current_file = []
             line = f.readline()
             while line:
-                current_file.append(line.split())
+                current_file.append([int(x) for x in line.split()])
                 line = f.readline()
             
-            y.append(current_file.pop(0))
-            X.append(current_file)
-    
+            y.append(current_file.pop(0)[0])
+            
+            aux = []
+            for i in range(len(current_file)):
+                for j in range(len(current_file[i])):
+                    aux.append(current_file[i][j])
+            X.append(aux)
+
     return X, y
 
 def update_weight(weight, learning_rate, desired_output, adaline_output, x):
     return weight + learning_rate * (desired_output - adaline_output) * x
 
-def adaline_train(X_train, y_train, learning_rate):
+def train(X_train, y_train, learning_rate):
+    weights = np.random.rand(len(X_train[0]))
+    max_iter = 10000
 
-def adaline_test(X_test, y_test):
+    for i in range(max_iter):
+        for j in range(len(X_train)):
+            sum = 0
+            for k in range(len(X_train[j])):
+                sum += X_train[j][k] * weights[k]
+            
+            output = 1 if sum >= 0 else -1    
+            if (output != y_train[j]):
+                for k in range(len(weights)):
+                    weights[k] = update_weight(weights[k], learning_rate, y_train[j], output, X_train[j][k])
+                break
+
+    return weights
+
+def test(X_test, y_test, weights):
+    for i in range(len(X_test)):
+        sum = 0
+        for j in range(len(X_test[i])):
+            sum += X_test[i][j] * weights[j]
+
+        output = 1 if sum >= 0 else -1
+        print("Esperado ", y_test[i], " - Saída do Adaline ", output)
 
 def main():
     X_train, y_train = read_files("train/")
     X_test, y_test = read_files("test/")
     
-    for i in range(6):
-        print(y_test[i])
-        for j in range(5):
-            print(X_test[i][j])
-        print("")
-    
-    learning_rate = 0.5
-    adaline_train(X_train, y_train, learning_rate)
-    adaline_test(X_test, y_test)
+    learning_rate = 0.000001
+    weights = train(X_train, y_train, learning_rate)
+    test(X_test, y_test, weights)
 
 if __name__ == '__main__':
     main()
