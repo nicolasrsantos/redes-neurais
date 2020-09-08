f9e7ba7d557c0700d48db44c7d1f714d8c2cc3b4
Nícolas Roque <nicolas.rsantos1@gmail.com>
2020-09-08 20:05:54 -0300

    Bias inserido

--- adaline/adaline.py
+++ adaline/adaline.py
@@ -26,28 +26,29 @@ def read_files(dir):
 def update_weight(weight, learning_rate, desired_output, adaline_output, x):
     return weight + learning_rate * (desired_output - adaline_output) * x
 
-def train(X_train, y_train, learning_rate):
-    weights = np.random.rand(len(X_train[0]))
+def train(X_train, y_train, learning_rate, bias):
+    weights = np.random.rand(len(X_train[0]) + 1)
     max_iter = 1000
 
     for i in range(max_iter):
         for j in range(len(X_train)):
-            sum = 0
+            sum = bias * weights[0]
             for k in range(len(X_train[j])):
-                sum += X_train[j][k] * weights[k]
+                sum += X_train[j][k] * weights[k + 1]
             
             output = 1 if sum >= 0 else -1    
             if (output != y_train[j]):
-                for k in range(len(weights)):
-                    weights[k] = update_weight(weights[k], learning_rate, y_train[j], output, X_train[j][k])
+                weights[0] = weights[0] + learning_rate * (y_train[j] - output) * bias
+                for k in range(len(X_train[j])):
+                    weights[k + 1] = update_weight(weights[k + 1], learning_rate, y_train[j], output, X_train[j][k])
     
     return weights
 
-def test(X_test, y_test, weights):
+def test(X_test, y_test, weights, bias):
     for i in range(len(X_test)):
-        sum = 0
+        sum = bias * weights[0]
         for j in range(len(X_test[i])):
-            sum += X_test[i][j] * weights[j]
+            sum += X_test[i][j] * weights[j + 1]
 
         output = 1 if sum >= 0 else -1
         print("Esperado ", y_test[i], " - Saída do Adaline ", output)
@@ -56,9 +57,10 @@ def main():
     X_train, y_train = read_files("train/")
     X_test, y_test = read_files("test/")
     
+    bias = -1
     learning_rate = 0.5
-    weights = train(X_train, y_train, learning_rate)
-    test(X_test, y_test, weights)
+    weights = train(X_train, y_train, learning_rate, bias)
+    test(X_test, y_test, weights, bias)
 
 if __name__ == '__main__':
     main()
